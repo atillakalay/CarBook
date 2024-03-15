a11da0a04cd9a6335a374f787e0a2d6a8bfcfe18
atillakalay <atilla.kalayy@gmail.com>
2024-03-15 21:33:21 +0300

    Add project files.

---
+++ Domain/Entities/CarFeature.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities
+{
+    public class CarFeature
+    {
+        public int CarFeatureId { get; set; }
+        public bool Available { get; set; }
+        public int CarId { get; set; }
+        public int FeatureId { get; set; }
+        public Car Car { get; set; }
+        public Feature Feature { get; set; }
+
+    }
+}
